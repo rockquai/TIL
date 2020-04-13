cda01afed0377047d1f2a55f814d7901325a994d
rockquai <rockquai@gmail.com>
2020-04-13 11:46:43 +0900

    Update. README, static contextType

--- react_context_GP/src/components/MemberList/MemberList.js
+++ react_context_GP/src/components/MemberList/MemberList.js
@@ -1,19 +1,44 @@
-import React from 'react';
+// import React from 'react';
+// import MemberContext from '../../context/MemberContext';
+// import MemberItem from '../MemberItem';
+
+// const MemberList = () => {	
+// 	return (
+// 		<ul>
+// 			<MemberContext.Consumer>
+// 				{
+// 					({ memberList : {items}, removeMember }) => 
+// 						items.map( (item) => 
+// 							<MemberItem key={item.id} item={item} removeMember={removeMember} /> )
+// 				}
+// 			</MemberContext.Consumer>
+// 		</ul>
+// 	);
+// }
+
+// export default MemberList;
+
+// ----------------------------------------------------------------------------------------
+// [static contextType 사용]
+import React, { Component } from 'react';
 import MemberContext from '../../context/MemberContext';
 import MemberItem from '../MemberItem';
 
-const MemberList = () => {	
-	return (
-		<ul>
-			<MemberContext.Consumer>
+class MemberList extends Component {
+	static contextType = MemberContext;	
+
+    render() {
+		const { memberList : {items}, removeMember } = this.context;
+
+        return (
+            <ul>
 				{
-					({ memberList : {items}, removeMember }) => 
-						items.map( (item) => 
-							<MemberItem key={item.id} item={item} removeMember={removeMember} /> )
+					items.map( (item) => 
+						<MemberItem key={item.id} item={item} removeMember={removeMember} /> )
 				}
-			</MemberContext.Consumer>
-		</ul>
-	);
+            </ul>
+        );
+    }
 }
 
 export default MemberList;
